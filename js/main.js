0c0c06e4932485947a912c5d254786c2ab12b2e0
Your Name <you@example.com>
2020-12-17 18:59:01 +0000

    added jquery to site files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $("#submitbutton").click(function() {
 	  document.getElementById("form1").reset();
    });
+   
+<script>
+var c = document.getElementById("homemessage");
+var ctx = c.getContext("2d");
+ctx.font = "30px Arial";
+ctx.strokeText("Hello World",10,50);
+</script>
