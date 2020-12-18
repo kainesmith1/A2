eddbe87e39c184bd79089cdbc99a9ee3980c9866
Your Name <you@example.com>
2020-12-18 00:06:49 +0000

    Removed jquery, added slideshow javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
+{
+var i = 0;
+var images = []
+var time = 3000;
+
+images[0] = 'images/grid1.jpg';
+images[1] = 'images/grid2.jpg';
+images[2] = 'images/grid3.jpg';
+
+function changeImg(){
+	document.slideimg.src = images[i];
+	
+	if(i < images.length - 1){
+		i++;
+	} else {
+		i = 0;
+	}
+	
+	setTimeout("changeImg()", time);
+	
+}
+
+window.onload = changeImg;
+}
+
 $("#submitbutton").click(function() {
 	  document.getElementById("form1").reset();
    });
-   
-<script>
-var c = document.getElementById("homemessage");
-var ctx = c.getContext("2d");
-ctx.font = "30px Arial";
-ctx.strokeText("Hello World",10,50);
-</script>
+
+// var c = document.getElementById("homemessage");
+// var ctx = c.getContext("2d");
+// ctx.font = "30px Arial";
+// ctx.strokeText("Hello World",10,50);
